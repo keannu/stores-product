e88bfe3703acd2995e496a7ae30635dab6560009
keannurim <keannu@fusedsoftware.com>
2026-08-12 20:36:00 +0800

    First commit for vue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -15,10 +16,25 @@ export default defineConfig({
             ],
         }),
         tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // Laravel's Blade files are used to reference assets,
+                    // so this tells Vue not to worry about resolving `src` for these elements
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
+        },
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
 });
